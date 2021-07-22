b50b4dcc1cc8e439234ecdac42057dc8c8a8caf4
FallBlackMagic <68982201+FallBlackMagic@users.noreply.github.com>
2021-07-23 00:37:32 +0800

    Update install.py

--- MOT_UI/install.py
+++ MOT_UI/install.py
@@ -2,3 +2,5 @@ import os
 os.system("python -m pip install paddlepaddle-gpu==2.1.1.post101 -f https://paddlepaddle.org.cn/whl/mkl/stable.html")
 os.system("python -m pip install -r requirements.txt")
 os.system("python setup.py install")
+os.system("unzip fairmot_final.zip")
+os.system("rm -rf fairmot_final.zip")
